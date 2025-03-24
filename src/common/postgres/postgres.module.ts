1091bfe365e8b569af6fd58ad9c4dba89763f9be
Sathya Swaroop <98945513+SSV96@users.noreply.github.com>
2025-03-24 22:35:51 +0530

    Update postgres.module.ts

--- src/common/postgres/postgres.module.ts
+++ src/common/postgres/postgres.module.ts
@@ -1,28 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { PostgresType } from 'src/config';
+import { getPostgresConfig } from './postgres.config';
 
 @Module({
   imports: [
     SequelizeModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        const { host, username, password, dbName, port } =
-          configService.get<PostgresType>('db.postgres');
-
-        return {
-          dialect: 'postgres',
-          host,
-          port,
-          username,
-          password,
-          database: dbName,
-          autoLoadModels: true,
-          synchronize: true,
-        };
-      },
+      useFactory: getPostgresConfig,
     }),
   ],
 })
